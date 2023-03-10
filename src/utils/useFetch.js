78b9596644d0cfb8daeb216d711b8110bb2fdbe9
Buvaneshwari <thirubuvana@gmail.com>
2023-03-10 22:17:55 +0530

    gitprofileview1

--- src/utils/useFetch.js
+++ src/utils/useFetch.js
@@ -4,18 +4,19 @@ import paginate from './paginate'
 
 const useFetch = () => {
    const [data, setData] = useState([]);
-   const [isLoading, setIsLoading] = useState(true)
-
-   useEffect(() => {
-      getUsers();
-   }, [])
-
+   const [isLoading, setIsLoading] = useState(true);
    async function getUsers() {
       const response = await fetch(BASE_URL);
       const data = await response.json();
       setData(paginate(data));
       setIsLoading(false)
    }
+
+
+   useEffect(() => {
+      getUsers();
+   }, [])
+
 
 
    return { data, isLoading }
